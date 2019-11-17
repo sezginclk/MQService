1d7d3a46b21c736148692b59abf10d647988a6f8
Sezgin Çolak <sezgincolak90@gmail.com>
2019-11-17 16:55:46 +0300

    MQService

---
+++ MQService/MQService.Exe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQService.Exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MQService mQService = new MQService();
+            mQService.InsertLog(1, "1243", "LogEkledim.");
+
+        }
+    }
+}
---
+++ MQService/MQService/Publisher.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQService
+{
+
+    internal class Publisher
+    {
+        private readonly RabbitMQService _rabbitMQService;
+        public Publisher(string queueName, string message)
+        {
+            _rabbitMQService = new RabbitMQService();
+
+            using (var connection = _rabbitMQService.GetRabbitMQConnection())
+            {
+                using (var chanel = connection.CreateModel())
+                {
+                    chanel.QueueDeclare(queueName, false, false, false, null);
+                    chanel.BasicPublish("", queueName, null, Encoding.UTF8.GetBytes(message));
+                }
+            }
+        }
+    }
+}
